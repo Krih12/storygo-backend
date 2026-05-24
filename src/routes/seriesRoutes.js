f2610d07411cb4beb63f313883e8472fcacef642
Krih12 <157201076+Krih12@users.noreply.github.com>
2026-05-24 15:54:16 +0530

    Update seriesRoutes.js

--- src/routes/seriesRoutes.js
+++ src/routes/seriesRoutes.js
@@ -4,14 +4,14 @@ const seriesController = require('../controllers/seriesController');
 const { authenticate, optionalAuth, authorizeCreator, authorizeOwner } = require('../middleware/authenticate');
 const { uploadImage } = require('../middleware/upload');
 
-// Public
+// Public routes
 router.get('/', optionalAuth, seriesController.getAllSeries);
 router.get('/featured', seriesController.getFeaturedSeries);
 router.get('/:id', optionalAuth, seriesController.getSeriesById);
 router.get('/:id/episodes', optionalAuth, seriesController.getSeriesEpisodes);
 router.post('/:id/rate', authenticate, seriesController.rateSeries);
 
-// Creator
+// Creator routes
 router.post('/', authenticate, authorizeCreator, uploadImage, seriesController.createSeries);
 router.put('/:id', authenticate, authorizeCreator, authorizeOwner('series'), uploadImage, seriesController.updateSeries);
 router.delete('/:id', authenticate, authorizeCreator, authorizeOwner('series'), seriesController.deleteSeries);
